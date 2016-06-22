39ca960b9a0ec2c405d4836e64f25bb257a0245b
danicormu <danicormu@gmail.com>
2016-06-22 12:46:38 -0600

    Some Resources

---
+++ Sniper_project/Assets/Global Resources/Global Scripts/PauseMenu.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+var PauseObject: GameObject;
+
+
+function Start () 
+{
+	PauseObject.SetActive(false);
+}
+
+function Update () 
+{
+	if(Input.GetKeyDown(KeyCode.Escape))
+	{
+		ChangeTimeScale();
+	}
+}
+
+function ChangeTimeScale()
+{
+	if(Time.timeScale == 1)
+		PauseGame();
+	else if(Time.timeScale == 0)
+		ContinueGame();
+}
+
+function PauseGame()
+{
+	PauseObject.SetActive(true);
+	Time.timeScale = 0;
+}
+
+function ContinueGame()
+{
+	PauseObject.SetActive(false);
+	Time.timeScale = 1;
+} 
---
+++ Sniper_project/Assets/Global Resources/Global Scripts/ResumeGameBtnScript.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var PauseObject: GameObject;
+
+function ResumeGameBtnClick()
+{
+    Time.timeScale = 1;
+    PauseObject.SetActive(false);
+} 
