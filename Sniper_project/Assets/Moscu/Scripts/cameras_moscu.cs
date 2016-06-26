a988ea800eed58c6624e230d304558955a7b02b2
danicormu <danicormu@gmail.com>
2016-06-25 22:22:37 -0600

    Minimapa, civiles, Personaje principal escenario 3, efecto sonoro
    # Conflicts:
    #	Sniper_project/Library/metadata/05/057dcbe9b7d3acb4ab1a4fc57a1fa6ec
    
    #	Sniper_project/Library/metadata/90/90fff7c8ca92e814aa542c3334f1490c.info
    #	Sniper_project/Library/metadata/a6/a6706c539f242fb439b78c280ed0cb08
    #	Sniper_project/Library/metadata/aa/aa475214362ae4640af97fc5ec051213
    #	Sniper_project/Library/metadata/cc/ccddafcfdc8f1864dae02b3ea4f7339f
    
    Examen
    
    Examen
    
    US - 23 Minimapa escenario
    US - 31 Diseño del personaje
    objetivo Mision 3
    US - 14 Efecto sonoro (rio)
    
    y otros de las tareas del
    Sprint

---
+++ Sniper_project/Assets/Moscu/Scripts/cameras_moscu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameras_moscu : MonoBehaviour {
+
+	public Camera cam1 = new Camera ();
+	public Camera cam2 = new Camera ();
+	public Camera cam3 = new Camera ();
+	public Camera cam4 = new Camera ();
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A)) {
+			cam1.enabled = true;
+			cam2.enabled = false;
+			cam3.enabled = false;
+			cam4.enabled = false;
+		} else if (Input.GetKey (KeyCode.S)) {
+			cam1.enabled = false;
+			cam2.enabled = true;
+			cam3.enabled = false;
+			cam4.enabled = false;
+		} else if (Input.GetKey (KeyCode.D)) {
+			cam1.enabled = false;
+			cam2.enabled = false;
+			cam3.enabled = true;
+			cam4.enabled = false;
+		} else if (Input.GetKey (KeyCode.S)) {
+			cam1.enabled = false;
+			cam2.enabled = false;
+			cam3.enabled = false;
+			cam4.enabled = true;
+		}
+	}
+}
