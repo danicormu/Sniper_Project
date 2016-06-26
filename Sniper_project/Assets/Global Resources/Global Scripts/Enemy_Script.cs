68419e68a43d46f6f052b31dc5dd23b76fa82533
Ermis Luna <ermis993@gmail.com>
2016-06-25 20:05:57 -0600

    Adding IA to Enemies
    Agregue enemigo con intelligencia artificial, produce dano, recibe dano,
    tiene animaciones de busqueda, disparo y muerte, sonido del enemigo al
    disparar, vida del jugador reflejada, pantalla parpadeante cuando el
    jugador recibe dano, Scripts.

---
+++ Sniper_project/Assets/Global Resources/Global Scripts/Enemy_Script.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Script : MonoBehaviour {
+
+	static Animator anim;
+	public int maxHealth = 10;
+
+	public int currentHealth;
+	public float chaseRange = 0.0f;
+	public float attackRange = 0.0f;
+	public float attackCD = 0.0f;
+	private float attackCDLeft;
+
+	AudioSource playerAudio;
+	public AudioClip deathClip;
+
+	private GameObject playerToKill; 
+	player playerHealth;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		currentHealth = maxHealth;
+		playerToKill = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = playerToKill.GetComponent<player> ();
+		playerAudio = GetComponent<AudioSource> ();
+		}
+
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (playerToKill.transform.position, this.transform.position) <= 60) {
+			Vector3 direction = playerToKill.transform.position - this.transform.position;
+			direction.y = 0;
+			this.transform.rotation = Quaternion.Slerp (this.transform.rotation, Quaternion.LookRotation (direction), 0.1f);
+			if (direction.magnitude > 40) {
+				this.transform.Translate (0, 0, 0.05f);
+				anim.SetBool ("Walk", true);
+			} else {
+				anim.SetTrigger ("Shoot");
+				anim.SetBool ("Walk", false);
+				attack ();
+			}
+		}else {
+			anim.SetTrigger ("PlayerDead");
+			anim.SetBool ("Walk", false);
+		}
+
+		if (attackCDLeft > 0.0f)
+			attackCDLeft -= Time.deltaTime;
+
+	}
+
+	public void loseHealth(int damage){
+		if(currentHealth > 0){
+			currentHealth -= damage;
+			playerAudio.Play ();
+		}if (currentHealth == 0) {
+			Die ();
+		}
+	}
+
+	public void attack(){
+		if(attackCDLeft <= 0.0f){
+			playerHealth.loseHealth (1);
+			attackCDLeft = attackCD;
+		}
+	}
+
+	void Die(){
+		anim.SetTrigger ("Dead");
+		playerAudio.clip = deathClip;
+		playerAudio.Play (); 
+		Destroy (this.gameObject, 5.0f);
+		}
+
+
+
+
+}
+
---
+++ Sniper_project/Assets/Global Resources/Global Scripts/player.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public float attackRange = 0.0f;
+	public float attackCD = 0.0f;
+	public int maxHealth = 0;
+	public int currentHealth;
+	public float attackCDLeft;
+	AudioSource playerAudio;
+	bool isDamaged;
+	bool isDeath;
+
+	public Slider healthSlider;
+	public Image damageImage;
+	public float flashSpeed = 5f;
+	public Color flashColor = new Color (1f, 0f, 0f, 0.1f);
+
+
+	public AudioClip deathClip;
+	// Use this for initialization
+	void Start () {
+		currentHealth = maxHealth;
+		playerAudio = GetComponent<AudioSource> ();
+		attackCDLeft = attackCD;
+		}
+
+	// Update is called once per frame
+	void Update () {
+		isDead ();
+
+		if (isDamaged && !isDeath) {
+			damageImage.color = flashColor;
+		} else {
+			damageImage.color = Color.Lerp (damageImage.color , Color.clear, flashSpeed * Time.deltaTime);
+		}
+
+		isDamaged = false;
+		isDeath = false;
+
+		attackEnemy ();
+	}		
+
+
+	public void attackEnemy(){
+		if (attackCDLeft > 0.0f)
+			attackCDLeft -= Time.deltaTime;
+
+		if (Input.GetMouseButtonDown (0)) {	
+			if (getNearEnemy () != null) {
+				if (Vector3.Distance (transform.position, getNearEnemy ().transform.position) <= attackRange) {
+						killEnemy ();
+						attackCDLeft = attackCD;	
+				}
+			}
+		}	
+	}
+
+	GameObject getNearEnemy(){
+		GameObject nearest = null;
+		foreach(GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy")){
+			if(nearest == null || Vector3.Distance(transform.position, enemy.transform.position) < Vector3.Distance(transform.position, nearest.transform.position)){
+				nearest = enemy;
+			}
+		}
+		return nearest;
+	}
+
+	public void loseHealth(int damage){
+	if (getNearEnemy().GetComponent<Enemy_Script> ().currentHealth > 0) {
+			isDamaged = true;
+			playerAudio.Play ();
+			currentHealth -= damage;
+			healthSlider.value = currentHealth;
+		}
+	}
+
+	void killEnemy(){
+		if(attackCDLeft <= 2.5f){
+			getNearEnemy ().GetComponent<Enemy_Script> ().loseHealth(5);
+		}
+
+	}
+
+	void isDead(){
+		if (currentHealth <= 0.0f) {
+			isDeath = true;
+			playerAudio.clip = deathClip;
+			playerAudio.Play ();
+			Destroy (this.gameObject);
+		}
+	}
+		
+	}
