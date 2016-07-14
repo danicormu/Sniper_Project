5bfa0d4fa22e1b3bde234124fe75e87baa498f2c
danicormu <danicormu@gmail.com>
2016-07-14 00:35:13 -0600

    Arreglo de radares

---
+++ Sniper_project/Assets/Global Resources/RadarMap/MakeRadarObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MakeRadarObject : MonoBehaviour {
+
+	public Image image;
+	// Use this for initialization
+	void Start () {
+		radarMap.RegisterRadarObject (this.gameObject, image);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDestroy(){
+		radarMap.RemoveRadarObjects (this.gameObject);
+	}
+}
