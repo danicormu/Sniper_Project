5c16053792f4bf16d34df02ac350ca33151e4935
maironcorrales <maironcorrales@gmail.com>
2016-08-05 23:58:06 -0600

    Winning Zone
    se agrego la zpne de "Win Level " en cada pantalla que basicamente
    consiste en tocar un objeto ya sea en la pantalla o en invisible ne el
    mundo osea llegar a un lugar

---
+++ Sniper_project/Assets/WinningZone.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WinningZone : MonoBehaviour {
+
+    public Collider player;
+    private bool triggered;
+
+    void OnTriggerEnter(Collider collider) 
+    {
+        if (collider != player)
+            return;
+        else 
+        {
+            triggered = true;
+            SceneManager.LoadScene("HeadQuarter");
+            MouseLock.MouseLocked = false;
+        }
+    }
+}
